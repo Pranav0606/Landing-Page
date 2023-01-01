ec82039e397bcc3185dad844fec8175fed1d3839
Pranav0606 <bavdhankarpranav1@gmail.com>
2023-01-01 21:29:31 +0530

    Added Clients section

---
+++ script.js
@@ -0,0 +1,29 @@
+let clients = document.querySelectorAll('.client');
+let clientImage = document.querySelectorAll('.client img');
+let imageContainer = document.getElementById("imagePreviewer");
+let srcs = new Array();
+console.log(srcs);
+let mainImageContainer = document.getElementsByClassName('imagePreviewerContainer');
+console.log(clientImage);
+
+
+// FontFace
+for(let i = 0; i<clientImage.length; i++)
+    srcs[i] = clientImage[i].src;
+
+function previewImg(a)
+{
+    let img = a.querySelector('img');
+    imageContainer.setAttribute('src', img.src);
+    mainImageContainer[0].style.opacity = '1';
+    mainImageContainer[0].style.top = 50 + '%';
+    // mainImageContainer[0].style.transform = 'scale(' + 1 + '), translate('-50 + '% ,' + 50 +  '%)';
+
+    console.log(a);
+}
+
+document.getElementById('delete').addEventListener('click', ()=>{
+    mainImageContainer[0].style.opacity = '0';
+    mainImageContainer[0].style.top = -100 + '%'; 
+    // mainImageContainer[0].style.left = 0 + '%';
+});
