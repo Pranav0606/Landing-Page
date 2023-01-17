04fc699e4db7cde18dad1cc8a66ae2484ef224f5
Pranav0606 <bavdhannkarpranav1@gmail.com>
2023-01-17 21:47:43 +0530

    Responsive

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let clients = document.querySelectorAll('.client');
 let clientImage = document.querySelectorAll('.client img');
 let imageContainer = document.getElementById("imagePreviewer");
+let list = document.querySelector('.listItems');
 let srcs = new Array();
 console.log(srcs);
 let mainImageContainer = document.getElementsByClassName('imagePreviewerContainer');
@@ -8,11 +9,10 @@ console.log(clientImage);
 
 
 // FontFace
-for(let i = 0; i<clientImage.length; i++)
+for (let i = 0; i < clientImage.length; i++)
     srcs[i] = clientImage[i].src;
 
-function previewImg(a)
-{
+function previewImg(a) {
     let img = a.querySelector('img');
     imageContainer.setAttribute('src', img.src);
     mainImageContainer[0].style.opacity = '1';
@@ -20,8 +20,18 @@ function previewImg(a)
     console.log(a);
 }
 
-document.getElementById('delete').addEventListener('click', ()=>{
+document.getElementById('delete').addEventListener('click', () => {
     mainImageContainer[0].style.opacity = '0';
-    mainImageContainer[0].style.top = -100 + '%'; 
-    // mainImageContainer[0].style.left = 0 + '%';
+    mainImageContainer[0].style.top = -100 + '%';
+});
+
+
+list.style.left = (parseInt(window.outerWidth) + 100) + 'px';
+document.getElementById("hamburger").addEventListener("click", () => {
+    if (list.style.left !== '0px')
+        list.style.left = 0 + 'px';
+    else
+        list.style.left = (parseInt(window.outerWidth) + 100) + 'px';
+
+
 });
