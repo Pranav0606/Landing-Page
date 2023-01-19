fa0643ac4da9adcefc3dd6b7022ec3c6f815c50b
Pranav0606 <bavdhannkarpranav1@gmail.com>
2023-01-19 14:01:32 +0530

    Added FAQ's

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 let list = document.querySelector('.listItems');
 
 document.getElementById("hamburger").addEventListener("click", () => {
-    list.classList.toggle('isVisible');
     if (list.getAttribute('id') === 'isVisible')
         list.setAttribute("id", "");
     else
